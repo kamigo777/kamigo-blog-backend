79aa99cace0771e6add7aa74bcc2860e5bf8fb79
Igor <64247014+kamigo777@users.noreply.github.com>
2022-10-30 16:31:18 +0100

    mkdirUploads

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import multer from 'multer';
 import cors from 'cors';
 
@@ -19,7 +20,12 @@ mongoose.connect(process.env.MONGODB_URI)
 const app = express();
 
 const storage = multer.diskStorage({
+
     destination:(_, __, cb) => {
+    if(!fs.existsSync('uploads')) {
+    fs.mkdirSync('uploads')
+}
+
         cb(null, 'uploads');
     },
     filename: (_, file, cb) => {
